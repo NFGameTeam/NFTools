5a2b617e924c19c96820e920b1091a7a43258612
tangyu1018@163.com <tangyu1018@163.com>
2016-04-06 15:02:28 +0800

    Add NFLoaderForWin

---
+++ NFLoaderForWin/frmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace NFLoaderForWin
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtConfigPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOpenConfig = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStartAll = new System.Windows.Forms.Button();
+            this.btnStopAll = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtConfigPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnOpenConfig);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(596, 57);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Config";
+            // 
+            // txtConfigPath
+            // 
+            this.txtConfigPath.Location = new System.Drawing.Point(113, 22);
+            this.txtConfigPath.Name = "txtConfigPath";
+            this.txtConfigPath.ReadOnly = true;
+            this.txtConfigPath.Size = new System.Drawing.Size(377, 21);
+            this.txtConfigPath.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "LoaderConfig";
+            // 
+            // btnOpenConfig
+            // 
+            this.btnOpenConfig.Location = new System.Drawing.Point(496, 20);
+            this.btnOpenConfig.Name = "btnOpenConfig";
+            this.btnOpenConfig.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenConfig.TabIndex = 9;
+            this.btnOpenConfig.Text = "Open";
+            this.btnOpenConfig.UseVisualStyleBackColor = true;
+            this.btnOpenConfig.Click += new System.EventHandler(this.btnOpenConfig_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtLog);
+            this.groupBox2.Location = new System.Drawing.Point(12, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(596, 213);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // btnStartAll
+            // 
+            this.btnStartAll.Location = new System.Drawing.Point(211, 305);
+            this.btnStartAll.Name = "btnStartAll";
+            this.btnStartAll.Size = new System.Drawing.Size(75, 23);
+            this.btnStartAll.TabIndex = 8;
+            this.btnStartAll.Text = "StartAll";
+            this.btnStartAll.UseVisualStyleBackColor = true;
+            this.btnStartAll.Click += new System.EventHandler(this.btnStartAll_Click);
+            // 
+            // btnStopAll
+            // 
+            this.btnStopAll.Location = new System.Drawing.Point(335, 305);
+            this.btnStopAll.Name = "btnStopAll";
+            this.btnStopAll.Size = new System.Drawing.Size(75, 23);
+            this.btnStopAll.TabIndex = 9;
+            this.btnStopAll.Text = "StopAll";
+            this.btnStopAll.UseVisualStyleBackColor = true;
+            this.btnStopAll.Click += new System.EventHandler(this.btnStopAll_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(27, 20);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(544, 179);
+            this.txtLog.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 364);
+            this.Controls.Add(this.btnStopAll);
+            this.Controls.Add(this.btnStartAll);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "NFLoader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtConfigPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOpenConfig;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStartAll;
+        private System.Windows.Forms.Button btnStopAll;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ NFLoaderForWin/frmMain.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace NFLoaderForWin
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            txtConfigPath.Text = Application.StartupPath + "\\NFLoaderConfig.xml";
+            if (System.IO.File.Exists(txtConfigPath.Text))
+            {
+                NFRoot = ReadNFRootFromXML(txtConfigPath.Text);
+            }
+            CheckConfig();
+        }
+
+        private string NFRoot;
+        private bool isConfigChecked = false;
+        private Dictionary<int, string> ProcessIDs = new Dictionary<int, string>();
+        private List<string> servers = new List<string>();
+
+        private void btnOpenConfig_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "NFLoaderConfig|*.xml";
+                ofd.FileName = txtConfigPath.Text;
+                ofd.ShowDialog();
+                if (ofd.FileName != txtConfigPath.Text)
+                {
+                    txtConfigPath.Text = ofd.FileName;
+                }
+
+                NFRoot = ReadNFRootFromXML(txtConfigPath.Text);
+                CheckConfig();
+            }
+
+
+        }
+
+        private void CheckConfig()
+        {
+            if (NFRoot != "")
+            {
+                if (System.IO.Directory.Exists(NFRoot))
+                {
+                    txtLog.AppendText("NFRoot:" + NFRoot + "\r\n");
+                    isConfigChecked = true;
+                }
+                else
+                {
+                    txtLog.AppendText("NFRoot:Error, Please Check Your NFRoot:" + NFRoot + "\r\n");
+                }
+            }
+            txtLog.SelectionStart = txtLog.TextLength;
+            txtLog.ScrollToCaret();
+        }
+
+        private string ReadNFRootFromXML(string text)
+        {
+            if (System.IO.File.Exists(text))
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(txtConfigPath.Text);    //加载Xml文件  
+                XmlElement rootElem = doc.DocumentElement;   //获取根节点  
+                XmlNodeList xmlNode = rootElem.GetElementsByTagName("NFRoot");
+                foreach (XmlNode node in xmlNode)
+                {
+                    string strName = ((XmlElement)node).GetAttribute("Path");
+                    return strName;
+                }
+            }
+            return "";
+        }
+
+        private void ReadServerConfigFromLogicClass(string logicClass)
+        {
+
+            if (System.IO.File.Exists(logicClass))
+            {
+                servers.Clear();
+                XmlDocument doc = new XmlDocument();
+                doc.Load(logicClass);    //加载Xml文件  
+                XmlElement rootElem = doc.DocumentElement;   //获取根节点  
+                XmlNodeList xmlNode = rootElem.GetElementsByTagName("Class");
+                foreach (XmlNode node in xmlNode)
+                {
+                    XmlNodeList sub2Nodes = ((XmlElement)node).GetElementsByTagName("Class");  //获取age子XmlElement集合  
+                    foreach (XmlNode node2 in sub2Nodes)
+                    {
+                        string strName = ((XmlElement)node2).GetAttribute("Id");
+                        if (strName == "Server")
+                        {
+                            string serverConfigPaht = ((XmlElement)node2).GetAttribute("InstancePath");
+                            XmlDocument docServer = new XmlDocument();
+                            docServer.Load(NFRoot + "\\_Out\\Server\\" + serverConfigPaht);
+                            XmlElement rootElemServ = docServer.DocumentElement;   //获取根节点  
+                            XmlNodeList xmlNodeServ = rootElemServ.GetElementsByTagName("Object");
+                            foreach (XmlNode serNode in xmlNodeServ)
+                            {
+                                string servName = ((XmlElement)serNode).GetAttribute("ID");
+                                servers.Add(servName);
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void btnStartAll_Click(object sender, EventArgs e)
+        {
+            if (!isConfigChecked) { return; }
+            ProcessIDs.Clear();
+            CopyDllFile();
+            int ServCount = 0;
+            string faildList = "";
+            //1.启动NFMasterServer
+            if (StartServer("NFMasterServer"))
+                ServCount++;
+            else
+                faildList += " NFMasterServer ";
+            Thread.Sleep(1000);
+
+            //2.启动NFWorldServer
+            if (StartServer("NFWorldServer"))
+                ServCount++;
+            else
+                faildList += " NFWorldServer ";
+            Thread.Sleep(1000);
+
+            //3.启动NFLoginServer
+            if (StartServer("NFLoginServer"))
+                ServCount++;
+            else
+                faildList += " NFLoginServer ";
+            Thread.Sleep(1000);
+
+            //4.启动NFGameServer
+            if (StartServer("NFGameServer1"))
+                ServCount++;
+            else
+                faildList += " NFGameServer1 ";
+            Thread.Sleep(1000);
+
+            //5.启动NFProxyServer
+            if (StartServer("NFProxyServer1"))
+                ServCount++;
+            else
+                faildList += " NFProxyServer1 ";
+
+            if (ServCount == 5)
+            {
+                txtLog.Text += "All Servers have been started!\r\n";
+            }
+            else
+            {
+                txtLog.Text += "Some server haven't been started!" + faildList + "\r\n";
+            }
+        }
+
+        private void CopyDllFile()
+        {
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\libmysql.dll", NFRoot + "\\_Out\\Server\\Debug\\NFGameServer1\\libmysql.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\libmysql.dll", NFRoot + "\\_Out\\Server\\Debug\\NFLoginServer\\libmysql.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\libmysql.dll", NFRoot + "\\_Out\\Server\\Debug\\NFMasterServer\\libmysql.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\libmysql.dll", NFRoot + "\\_Out\\Server\\Debug\\NFProxyServer1\\libmysql.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\libmysql.dll", NFRoot + "\\_Out\\Server\\Debug\\NFWorldServer\\libmysql.dll", true);
+
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\mysqlpp_d.dll", NFRoot + "\\_Out\\Server\\Debug\\NFGameServer1\\mysqlpp_d.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\mysqlpp_d.dll", NFRoot + "\\_Out\\Server\\Debug\\NFLoginServer\\mysqlpp_d.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\mysqlpp_d.dll", NFRoot + "\\_Out\\Server\\Debug\\NFMasterServer\\mysqlpp_d.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\mysqlpp_d.dll", NFRoot + "\\_Out\\Server\\Debug\\NFProxyServer1\\mysqlpp_d.dll", true);
+            System.IO.File.Copy(NFRoot + "Dependencies\\lib\\Debug\\mysqlpp_d.dll", NFRoot + "\\_Out\\Server\\Debug\\NFWorldServer\\mysqlpp_d.dll", true);
+        }
+
+        private bool StartServer(string v)
+        {
+            string serverFileName = NFRoot + "\\_Out\\Server\\Debug\\" + v + "\\NFPluginLoader_d.exe";
+            string serverFilePath = NFRoot + "\\_Out\\Server\\Debug\\" + v;
+            if (!System.IO.File.Exists(serverFileName))
+            {
+                return false;
+            }
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = serverFileName;
+                //psi.UseShellExecute = false;
+                psi.WorkingDirectory = serverFilePath;
+                //psi.CreateNoWindow = true;
+
+                var sPro = Process.Start(psi);
+                ProcessIDs.Add(sPro.Id, v);
+
+                txtLog.Text += v + " [" + sPro.Id + "] started!\r\n";
+                txtLog.SelectionStart = txtLog.TextLength;
+                txtLog.ScrollToCaret();
+                sPro.EnableRaisingEvents = true;//设置进程终止时触发的时间
+                sPro.Exited += new EventHandler(myprocess_Exited);//发现外部程序关闭即触发方法myprocess_Exited
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+
+        }
+
+        private void ReadServerConfig()
+        {
+            string logicClass = "\\_Out\\Server\\NFDataCfg\\Struct\\LogicClass.xml";
+            ReadServerConfigFromLogicClass(NFRoot + logicClass);
+        }
+
+        private void myprocess_Exited(object sender, EventArgs e)//被触发的程序
+        {
+            lock (txtLog)
+            {
+                try
+                {
+                    txtLog.Text += ProcessIDs[((System.Diagnostics.Process)sender).Id] + " [" + ((System.Diagnostics.Process)sender).Id + "] close\r\n";
+                    txtLog.SelectionStart = txtLog.TextLength;
+                    txtLog.ScrollToCaret();
+                }
+                catch
+                {
+                    return;
+                }
+            }
+        }
+
+        private void btnStopAll_Click(object sender, EventArgs e)
+        {
+            foreach (var serv in ProcessIDs.Keys)
+            {
+                Process.GetProcessById(serv).Kill();
+            }
+            ProcessIDs.Clear();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var serv in ProcessIDs.Keys)
+            {
+                Process.GetProcessById(serv).Kill();
+            }
+            ProcessIDs.Clear();
+        }
+    }
+}
